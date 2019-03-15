433fb7dad141668f39d807515a79b9f12ea6c9cb
vikotodorov02@gmail.com <vikotodorov02@gmail.com>
2019-03-15 21:46:32 +0200

    navbara donapravih

--- signup.php
+++ signup.php
@@ -13,28 +13,28 @@
 	<div class="title" style = "margin-bottom:5%">
 		<h1>MeNa</h1>
 	</div>
-<nav class="navbar navbar-expand-sm bg-success navbar-dark fixed-bottom">
-  <ul class="navbar-nav">
-	<li class="nav-item">
-      <a class="nav-link active" href="index.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="Pharmacy_reg.html">Pharmacy</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="Hospital_reg.html">Doctor</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="signup.php">Patient</a>
-    </li>
-  </ul>
-</nav>
+	<nav class="navbar navbar-expand-sm bg-success navbar-dark fixed-bottom">
+  		<ul class="navbar-nav">
+			<li class="nav-item">
+      			<a class="nav-link" href="index.html">Home</a>
+   			</li>
+    		<li class="nav-item">
+      			<a class="nav-link" href="pharmacy_reg.html">Pharmacy</a>
+    		</li>
+   			<li class="nav-item">
+      			<a class="nav-link" href="doctor_reg.html">Doctor</a>
+    		</li>
+    		<li class="nav-item">
+      			<a class="nav-link active" href="signup.html">Patient</a>
+    		</li>
+  		</ul>
+	</nav>
 <div style = "text-align:center">
 <div class="container" >
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
-				<h3>Sign In</h3>
+				<h3>Sign Up</h3>
 			</div>
 			<div class="card-body">
 				<form style= "text-align:center" action = "includes/login.inc.php" method ="post">
@@ -76,52 +76,5 @@
 		</div>
 	</div>
 </div>
-<!--<div style = "text-align:center">
-<div class="container" >
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign Up</h3>
-			</div>
-			<div class="card-body">
-        <form  style= "text-align:center" action = "includes/signup.inc.php" method ="post">
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-user"></i></span>
-              </div>
-              <input class="form-control" type="text" placeholder="Enter Name" name="uname" required><br>
-            </div>
-            <div class="input-group form-group">
-  						<div class="input-group-prepend">
-  							<span class="input-group-text"><i class="fas fa-key"></i></span>
-  						</div>
-  						<input class="form-control" type="text" placeholder="Enter e-mail" name="email" required><br>
-  					</div>
-            <div class="input-group form-group">
-  						<div class="input-group-prepend">
-  							<span class="input-group-text"><i class="fas fa-key"></i></span>
-  						</div>
-  						<input class="form-control" type="password" placeholder="Enter Password" name="psw" required><br>
-  					</div>
-            <div class="input-group form-group">
-  						<div class="input-group-prepend">
-  							<span class="input-group-text"><i class="fas fa-key"></i></span>
-  						</div>
-  						<input class="form-control" type="password" placeholder="Repeat password" name="reppsw" required><br>
-  					</div>
-            <div class="input-group form-group">
-  						<div class="input-group-prepend">
-  							<span class="input-group-text"><i class="fas fa-key"></i></span>
-  						</div>
-  						<input class="form-control" type="text" placeholder="Enter Your PID" name="pid" required><br>
-  					</div>
-            <div class="form-group">
-  							<button class="btn float-right login_btn" type="submit" name="register-submit">Register</button>
-  					</div>
-        </form>
-			</div>
-		</div>
-	</div>
-</div>-->
 </body>
 </html>
