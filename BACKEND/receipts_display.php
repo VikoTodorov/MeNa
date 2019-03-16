60550ac30bb105eb13cd870a4619e5be1e8f92aa
alexuzunov <aleks_uzunov123@abv.bg>
2019-03-16 17:55:35 +0200

    Add files via upload

--- BACKEND/receipts_display.php
+++ BACKEND/receipts_display.php
@@ -16,34 +16,36 @@
 	</script>
 </head>
 <body class="background">
-	<div class="title" style = "margin-bottom:5%">
-		<h1>MeNa</h1>
-	</div>
-	<nav class="navbar navbar-expand-sm bg-success navbar-dark fixed-bottom">
-	  <ul class="navbar-nav">
-		<li class="nav-item">
-		  <a class="nav-link active" href="index.php">Home</a>
-		</li>
-		<li class="nav-item">
-		  <a class="nav-link" href="pharmacy.php">Pharmacy</a>
-		</li>
-		<li class="nav-item">
-		  <a class="nav-link" href="doctor.php">Doctor</a>
-		</li>
-		<li class="nav-item">
-		  <a class="nav-link" href="signup.php">Patient</a>
-		</li>
-	  </ul>
+	<nav class="top">
+		<span class="title">MeNa</span>
+		<ul class="my_navbar">
+			<li class="btn">
+				<a class="active" href="receipts_display.php">Logged</a>
+			</li>
+			<li class="btn">
+				<a class="link" href="pharmacy_reg.php">Pharmacy</a>
+			</li>
+			<li class="btn">
+				<a class="link" href="doctor_reg.php">Doctor</a>
+			</li>
+			<li class="btn">
+				<a class="link" href="patient_reg.php">Patient</a>
+			</li>
+			<li class="btn">
+				<a class="link" href="/includes/logout.inc.php">Log out</a>
+			</li>
+		</ul>
 	</nav>
 	<div style = "text-align:center">
 		<h1 style="color: white">YOUR RECIPES:</h1>
 		<?php
+			$n = 0;
 			require "includes/dbhandler.inc.php";
 			$patientid = $_SESSION['userId'];
-			$query = mysqli_query($conn, "SELECT * FROM receipts WHERE patientID = $patientID") or die(mysqli_error($conn));
+			$query = mysqli_query($conn, "SELECT * FROM receipts WHERE patientID=$patientid") or die(mysqli_error($conn));
 			while ($row = mysqli_fetch_array($query)){
 				echo "
-				<p style = 'color: white'>{$row['receiptContent']}</p>\n
+				<p style = 'color: white'>-{$row['receiptContent']}</p>\n
 				";
 			}
 		?>
